ece9449e7424173758bf25cad1fb3703fd4a916d
Luuk <luukbrauckmann@gmail.com>
2022-12-16 16:05:26 +0100

    input error fix

--- components/registration/registration.js
+++ components/registration/registration.js
@@ -1,2 +1,2 @@
-const registrationInputs = document.querySelectorAll('input')
+var registrationInputs = document.querySelectorAll('input')
 for(let input of registrationInputs) input.addEventListener('blur', () => input.dataset['touched'] = true)
--- components/sign-in/sign-in.js
+++ components/sign-in/sign-in.js
@@ -1,2 +1,2 @@
-const signInInputs = document.querySelectorAll('input')
+var signInInputs = document.querySelectorAll('input')
 for(let input of signInInputs) input.addEventListener('blur', () => input.dataset['touched'] = true)
