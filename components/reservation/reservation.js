596be436c71678dff9dfbe3a9b3cdf07fbc65cc1
Luuk <luukbrauckmann@gmail.com>
2022-12-21 15:00:42 +0100

    Navigation fix Styling voor desktop

--- components/reservation/reservation.js
+++ components/reservation/reservation.js
@@ -1,26 +1,38 @@
-selectedView = 'map'
+var views = [
+	{ label: 'Kaart', alias: 'kaart', value: 'map' },
+	{ label: 'Lijst', alias: 'lijst', value: 'list' }
+]
+var selectedView
 
 getView = async () => {
-	console.log();
-	const html = await fetch(`components/${selectedView}/${selectedView}.html`)
+	const html = await fetch(`components/${selectedView.value}/${selectedView.value}.html`)
 		.then((response) => response.text())
 
 	const content = document.getElementById('router-outlet')
 	content.innerHTML = html
 }
 
-changeView = (view) => {
+changeView = () => {
+	let viewName = window.location.hash.replace('#', '')
+	if (!viewName) viewName = 'kaart'
+	const view = views.find((viewsItem) => viewsItem.alias === viewName)
 	selectedView = view
+
 	const selectButtons = document.getElementsByClassName('select-buttons')[0]
 	const buttons = selectButtons.getElementsByTagName('button')
-	for(let button of buttons) button.dataset['active'] = button.dataset['view'] === selectedView
+	for(let button of buttons) button.dataset['active'] = button.dataset['view'] === view.value
+
 	getView()
+
+	window.history.pushState(null, null, `#${view.alias}`)
 }
 
 document.addEventListener('click', (event) => {
 	const { target } = event
 	if (!target.matches('button')) return
-	changeView(target.dataset.view)
+	const view = views.find((viewsItem) => viewsItem.value === target.dataset.view)
+	window.history.pushState(null, null, `#${view.alias}`)
+	changeView()
 })
 
-changeView('map')
+changeView()
