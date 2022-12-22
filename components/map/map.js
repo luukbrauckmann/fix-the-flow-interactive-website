7fdf1c51eb2d751aebc486131cfb9b638948a7e2
Luuk <luukbrauckmann@gmail.com>
2022-12-22 23:01:48 +0100

    Leaflet +

--- components/map/map.js
+++ components/map/map.js
@@ -1,4 +1,3 @@
-var map;
 var settings = {
 	center: { lat: 52.2434979, lng: 5.6343227 },
 	zoom: 7,
@@ -6,14 +5,6 @@ var settings = {
 	zoomControl: true
 }
 
-/**
- * Initializes map with Google Maps API
- */
-initMap = () => {
-  map = new google.maps.Map(document.getElementById('map'), settings)
-}
+var map = L.map('map').setView([52.2434979, 5.6343227], 7)
 
-onInit = async () => {
-	initMap()
-}
-onInit()
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 19 }).addTo(map)
